c4836fcffd3ac18225009108524595212c163edc
Bianca <biancalway@gmail.com>
2021-02-03 12:10:51 -0300

    Rspec: test + view + controller + model for basic functionality - create a todo.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,20 @@
 class TodosController < ApplicationController
     def index
+        @todos = Todo.all    # to allow show all todos in list
+    end
+
+    def new
+        @todo = Todo.new     # create an instance of todo
+    end
+
+    def create               # create a new todo with its params and redirect
+        Todo.create(todo_params)
+        redirect_to todos_path
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:title)
     end
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+    scenario "Successfully" do
+        visit root_path
+        
+        click_on "Add new"
+        fill_in "Title", with: "Buy groceries"
+        click_on "Submit"
+
+        expect(page).to have_css '.todos li', text:'Buy groceries'
+    end
+end
